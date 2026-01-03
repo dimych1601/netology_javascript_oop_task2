0f7b2f3ff6fb7227f823449d5101c38f888a77e2
ddmas <dimych1601@gamil.com>
2026-01-03 14:29:52 +0300

    Finish task2

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
-import sum from './basic';
+import Bowman from './Bowman'
+import Character from './Character'
+import Daemon from './Daemon'
+import Magician from './Magician'
+import Swordsman from './Swordsman'
+import Undead from './Undead'
+import Zombie from './Zombie'
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+export {
+  Bowman, Character, Daemon, Magician, Swordsman, Undead,
+  Zombie
+}
